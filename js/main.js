157986a55a63ce41c744b71107ca53452298c620
Piotr Stachowicz <ptr.stachowicz@gmail.com>
2020-04-25 19:54:43 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(".buttonBig").click(function () {
+    $(".journey").toggleClass('white');
+    $(".front").toggleClass('white');
+    $(".blackBgcStars").toggleClass('starsAll');
+    $(".blackBgcTwink").toggleClass('twinkling');
+    $(".circle").toggleClass('animationCircleOut');
+    $(".buttonBig").toggleClass('white');
+    $(".buttonBig").toggleClass('bgcTrans');
+});
